f95ebc5e4678011c4287c8f83f0f775f69efb31b
dgold01 <d.l.gold@hotmail.com>
2023-09-20 10:33:36 +0100

    Test

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,8 +12,8 @@ terraform {
 
 provider "aws" {
   region     = "eu-north-1" # Replace with your desired region
-  access_key = env("AWS_ACCESS_KEY_ID")
-  secret_key = env("AWS_SECRET_ACCESS_KEY")
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
   # Optional: If assuming a role
 }
 
